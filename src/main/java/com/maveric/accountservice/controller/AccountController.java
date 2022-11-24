c5fb9458b096609b106d147d04b22ed08bbe8cdc
Pavithra Venkatesan <pavithraven@maveric-systems.com>
2022-11-24 10:49:54 +0530

    create account service validation

---
+++ src/main/java/com/maveric/accountservice/controller/AccountController.java
@@ -0,0 +1,76 @@
+package com.maveric.accountservice.controller;
+
+import com.maveric.accountservice.dto.AccountDto;
+import com.maveric.accountservice.dto.BalanceDto;
+import com.maveric.accountservice.dto.UserDto;
+import com.maveric.accountservice.service.AccountService;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+import static com.maveric.accountservice.constants.Constants.AUTH_HEADER_ERROR_MESSAGE;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class AccountController {
+
+    //private static final Logger log = org.slf4j.LoggerFactory.getLogger(AccountController.class);
+
+    @Autowired
+    AccountService accountService;
+
+    /* Dummy Returns list of all accounts */
+    @GetMapping("customers/{customerId}/account")
+    public ResponseEntity<List<AccountDto>> getAccounts(@PathVariable String customerId, @RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "10") Integer pageSize) {
+        List<AccountDto> accountDtoResponse = accountService.getAccounts(page, pageSize);
+        return new ResponseEntity<>(accountDtoResponse, HttpStatus.OK);
+    }
+
+    /* Returns list of all valid accounts belonging a particular Customer Id */
+    @GetMapping("customers/{customerId}/accounts")
+    public ResponseEntity<List<AccountDto>> getAccountByCustomerId(@PathVariable String customerId, @RequestParam(defaultValue = "0") Integer page,
+                                                                   @RequestParam(defaultValue = "5") Integer pageSize) {
+        List<AccountDto> accountDtoResponse = accountService.getAccountByUserId(page, pageSize, customerId);
+        return new ResponseEntity<>(accountDtoResponse, HttpStatus.OK);
+
+    }
+
+
+    /* Creates a valid account */
+    @PostMapping("customers/{customerId}/accounts")
+    public ResponseEntity<AccountDto> createAccount(@PathVariable String customerId, @Valid @RequestBody AccountDto accountDto) {
+
+        AccountDto accountDtoResponse = accountService.createAccount(customerId, accountDto);
+        return new ResponseEntity<>(accountDtoResponse, HttpStatus.CREATED);
+
+    }
+
+    /* Returns Account details along with Balance details based on valid Account Id */
+    @GetMapping("customers/{customerId}/accounts/{accountId}")
+    public ResponseEntity<AccountDto> getAccountDetails(@PathVariable String customerId, @PathVariable String accountId) {
+
+        AccountDto accountDtoResponse = accountService.getAccountDetailsById(accountId);
+        return new ResponseEntity<>(accountDtoResponse, HttpStatus.OK);
+    }
+
+    /* Updates account details based on valid Account Id */
+    @PutMapping("customers/{customerId}/accounts/{accountId}")
+    public ResponseEntity<AccountDto> updateAccount(@PathVariable String customerId, @PathVariable String accountId, @RequestBody AccountDto accountDto) {
+        AccountDto accountDtoResponse = accountService.updateAccountDetails(customerId, accountId, accountDto);
+        return new ResponseEntity<>(accountDtoResponse, HttpStatus.OK);
+
+    }
+
+    /* Deletes account along with its corresponding balance and transaction details based on valid Account Id */
+    @DeleteMapping("customers/{customerId}/accounts/{accountId}")
+    public ResponseEntity<String> deleteAccount(@PathVariable String customerId, @PathVariable String accountId) {
+        String result = accountService.deleteAccount(accountId);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
